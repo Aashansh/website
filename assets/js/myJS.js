64ce271f604e7c7cc7dfdea5b9f50291a41afa91
vasu14115 <vasu14115@iiitd.ac.in>
2016-05-07 11:07:26 +0530

    minor changes

--- assets/js/myJS.js
+++ assets/js/myJS.js
@@ -63,22 +63,6 @@ checkScrollForParallax = debounce(function(){
 }, 6);
 
 
-//JS for auto slide change
-$(document).ready(function(){
-
-    $.material.init();
-    $('[data-toggle="tooltip"], [rel="tooltip"]').tooltip();
-    if($('.datepicker').length != 0){
-        $('.datepicker').datepicker({
-             weekStart:1
-        });
-    }
-    $('[data-toggle="popover"]').popover();
-  $('.carousel').carousel({
-      interval: 400000
-    });
-
-});
 
 
 
