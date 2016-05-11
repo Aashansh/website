a09e17a1a06c43ef2f9fb84e31d70603308a7d57
vasu14115 <vasu14115@iiitd.ac.in>
2016-05-11 13:07:40 +0530

    Some minor changes

--- assets/js/myJS.js
+++ assets/js/myJS.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
       $(".navbar-fixed-top").css("WebkitTransitionDuration","2s");
       $(".Aashansh-Brandname").css("WebkitTransitionDuration","2s");
       $(".Aashansh-Header-logo").css("WebkitTransitionDuration","2s");
-      $(".navbar-fixed-top").css("background-color", "#4b4d52");
+      $(".navbar-fixed-top").css("background-color", "#ffffff");
       $(".navbar-fixed-top").css("z-index", "16");
       $(".navbar-fixed-top").css("padding", "0px");
       $(".navbar-fixed-top").css("height", "60px");
@@ -54,49 +54,3 @@ function hideDiv() {
    document.getElementById('welcomeDiv').style.display = "none";
    document.getElementById('Show-about-us').style.display = "block";
 }
-
-
-//JS For Parallax
-$().ready(function(){
-  window_width = $(window).width();
-
-  if (window_width >= 768){
-    big_image = $('.wrapper > .header');
-
-      $(window).on('scroll', checkScrollForParallax);
-    }
-
-  });
-
-checkScrollForParallax = debounce(function(){
-  var current_scroll = $(this).scrollTop();
-
-  ValueForParallax = ($(window).scrollTop() / 3);
-  big_image.css({
-  'transform':'translate3d(0,' + ValueForParallax +'px,0)',
-  '-webkit-transform':'translate3d(0,' + ValueForParallax +'px,0)',
-  '-ms-transform':'translate3d(0,' + ValueForParallax +'px,0)',
-  '-o-transform':'translate3d(0,' + ValueForParallax +'px,0)'
-  });
-
-}, 6);
-
-
-
-
-
-
-//The Debounce Function 
-function debounce(func, wait, immediate) {
-  var timeout;
-  return function() {
-    var context = this, args = arguments;
-    clearTimeout(timeout);
-    timeout = setTimeout(function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    }, wait);
-    if (immediate && !timeout) func.apply(context, args);
-  };
-};
-
