d99074be3d9ee8299dfc481a753fa04a55330300
vasu14115 <vasu14115@iiitd.ac.in>
2016-05-25 23:18:25 +0530

    Minor Changes

--- assets/js/angular_myJS.js
+++ assets/js/angular_myJS.js
@@ -9,24 +9,12 @@ Forms.controller('Login',['$scope','$window',function($scope,$window){
 	var error_uname = "";
 	var error_pword = "";
 
-	$scope.link = "login.html";
+	$scope.link = "index.html";
 	$scope.$watch('username', function(val) {
         if (val) {
         	console.log($scope.link);
             login_uname = val;
         }
-
-        if (login_uname.length > 4) {
-    		if(flag_pword===1){
-    			$scope.link = "index.html";
-			}
-    		$scope.error_uname = "";
-    		flag_uname = 1;
-    	}else{
-    		$scope.error_uname = "Username Must be more than 4 characters";
-    		$scope.link = "login.html";
-    		flag_uname = 0;
-    	}
 
     });
 
@@ -60,9 +48,7 @@ Forms.controller('Signup',['$scope','$window',function($scope,$window){
 	var flag_uname=false;
 	var flag_pword=false;;
 	var flag_email=false;
-	var error_uname = "";
-	var error_pword = "";
-	var error_email="";
+
 	var EMAIL_REGEXP = /^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
 
 	$scope.link = "signup.html";
@@ -72,17 +58,7 @@ Forms.controller('Signup',['$scope','$window',function($scope,$window){
             signup_uname = val;
         }
 
-        if (signup_uname.length > 4) {
-    		if(flag_pword===true && flag_email===true){
-    			$scope.link = "login.html";
-			}
-    		$scope.error_uname = "";
-    		flag_uname = true;
-    	}else{
-    		$scope.error_uname = "Username Must be more than 4 characters";
-    		$scope.link = "signup.html";
-    		flag_uname = false;
-    	}
+        
 
     });
 
@@ -90,17 +66,7 @@ Forms.controller('Signup',['$scope','$window',function($scope,$window){
     	if (val){
     		signup_pword = val;
     	}
-    	if (signup_pword.length > 6) {
-    		if(flag_uname===true && flag_email===true){
-    			$scope.link = "login.html";
-			}
-    		$scope.error_pword = "";
-    		flag_pword = true;
-    	}else{
-    		$scope.error_pword = "Password Must be more than 6 characters";
-    		$scope.link = "signup.html";
-    		flag_pword = false;
-    	}
+    	
 
     });
 
@@ -109,20 +75,52 @@ Forms.controller('Signup',['$scope','$window',function($scope,$window){
     		signup_email = val;
     	}
 
-    	var isMatchRegex = EMAIL_REGEXP.test(signup_email);
-    	if (isMatchRegex) {
-    		if(flag_uname===true && flag_pword===true){
-    			$scope.link = "login.html";
-			}
-    		$scope.error_email = "";
-    		flag_email = true;
-    	}else{
-    		$scope.error_email = "Invalid Email";
-    		$scope.link = "signup.html";
-    		flag_email = false;
-    	}
+    	
 
     });
+
+    $scope.validate = function(){
+        if (signup_uname.length > 4) {
+            if(flag_pword===true && flag_email===true){
+                $scope.link = "login.html";
+            }
+            $scope.error_uname = "";
+            flag_uname = true;
+        }else{
+            $scope.error_uname = "Username Must be more than 4 characters";
+            $scope.link = "#";
+            flag_uname = false;
+        }
+
+        if (signup_pword.length > 6) {
+            if(flag_uname===true && flag_email===true){
+                $scope.link = "login.html";
+            }
+            $scope.error_pword = "";
+            flag_pword = true;
+        }else{
+            $scope.error_pword = "Password Must be more than 6 characters";
+            $scope.link = "#";
+            flag_pword = false;
+        }
+
+        var isMatchRegex = EMAIL_REGEXP.test(signup_email);
+        if (isMatchRegex) {
+            if(flag_uname===true && flag_pword===true){
+                $scope.link = "login.html";
+            }
+            $scope.error_email = "";
+            flag_email = true;
+        }else{
+            $scope.error_email = "Invalid Email";
+            $scope.link = "#";
+            flag_email = false;
+        }
+
+
+
+
+    }
 
     
 
